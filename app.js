485a9a9603b85702f07790fc6c677f8564846c5c
Ethan O'Connor <55208345+ethanoc123@users.noreply.github.com>
2022-06-20 00:36:48 +0100

    Update app.js

--- app.js
+++ app.js
@@ -53,20 +53,3 @@ const hideMobileMenu = () => {
 
 menuLinks.addEventListener("click", hideMobileMenu);
 navLogo.addEventListener("click", hideMobileMenu);
-
-var app = document.getElementById("app");
-
-var typewriter = new Typewriter(app, {
-  loop: true,
-});
-
-typewriter
-  .typeString("Hello World!")
-  .pauseFor(2500)
-  .deleteAll()
-  .typeString("Strings can be removed")
-  .pauseFor(2500)
-  .deleteChars(7)
-  .typeString("<strong>altered!</strong>")
-  .pauseFor(2500)
-  .start();
